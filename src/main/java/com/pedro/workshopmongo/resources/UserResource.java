cc926e48c9cb3ac1c06bf6e3eb3f424354b7f8ed
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-02-10 10:43:43 -0300

    Entity user and REST

---
+++ src/main/java/com/pedro/workshopmongo/resources/UserResource.java
@@ -0,0 +1,25 @@
+package com.pedro.workshopmongo.resources;
+
+import com.pedro.workshopmongo.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<User>> findAll() {
+        User pedro = new User("1", "Pedro", "pedro@gmail.com");
+        User alex = new User("2", "Alex", "alex@gmail.com");
+        List<User> list = new ArrayList<User>(Arrays.asList(pedro, alex));
+        return ResponseEntity.ok().body(list);
+    }
+
+}
