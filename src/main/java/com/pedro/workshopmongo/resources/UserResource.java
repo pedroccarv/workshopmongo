184a5091e51e551ad979e8829e4cc3f87b67b350
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-02-10 14:30:24 -0300

    feat: implement findById with exception handling

--- src/main/java/com/pedro/workshopmongo/resources/UserResource.java
+++ src/main/java/com/pedro/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import com.pedro.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,4 +30,10 @@ public class UserResource {
         return ResponseEntity.ok().body(listDto);
     }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDto> findById(@PathVariable String id) {
+        User user = service.findById(id);
+        UserDto userDto = new UserDto(user);
+        return ResponseEntity.ok().body(userDto);
+    }
 }
--- src/main/java/com/pedro/workshopmongo/services/UserService.java
+++ src/main/java/com/pedro/workshopmongo/services/UserService.java
@@ -3,10 +3,12 @@ package com.pedro.workshopmongo.services;
 import com.pedro.workshopmongo.domain.User;
 import com.pedro.workshopmongo.repository.UserRepository;
 import com.pedro.workshopmongo.resources.UserResource;
+import com.pedro.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> user = repo.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException(id));
     }
 }
