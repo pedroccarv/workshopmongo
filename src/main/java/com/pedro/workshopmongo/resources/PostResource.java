d20582a6f985864b901736bf63fd9436f8d07b4e
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-02-11 11:51:17 -0300

    feat: implement title search using query methods

--- src/main/java/com/pedro/workshopmongo/resources/PostResource.java
+++ src/main/java/com/pedro/workshopmongo/resources/PostResource.java
@@ -2,13 +2,15 @@ package com.pedro.workshopmongo.resources;
 
 import com.pedro.workshopmongo.domain.Post;
 import com.pedro.workshopmongo.repository.PostRepository;
+import com.pedro.workshopmongo.resources.util.URL;
 import com.pedro.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -22,4 +24,10 @@ public class PostResource {
         return ResponseEntity.ok().body(service.findById(id));
     }
 
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findbyTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
 }
--- src/main/java/com/pedro/workshopmongo/services/PostService.java
+++ src/main/java/com/pedro/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.pedro.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,6 +17,10 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> post = repo.findById(id);
         return post.orElseThrow(() -> new ObjectNotFoundException(id));
+    }
+
+    public List<Post> findbyTitle(String title) {
+        return repo.findByTitleContainingIgnoreCase(title);
     }
 
 }
